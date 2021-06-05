c6a81a0dfe994375c5cffeb90a85747036bea459
Alano Terblanche <alanoterblanche@gmail.com>
2021-06-05 20:03:04 +0200

    feat: cli

--- go.mod
+++ go.mod
@@ -7,8 +7,8 @@ require (
 	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/knadh/koanf v1.0.0
 	github.com/leodido/go-urn v1.2.1 // indirect
-	github.com/stretchr/testify v1.6.1
-	golang.org/x/sys v0.0.0-20210525143221-35b2ab0089ea // indirect
+	github.com/stretchr/testify v1.7.0
+	github.com/urfave/cli/v2 v2.3.0
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0
 )
--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"github.com/Oleaintueri/Conjure/pkg/conjure"
 	"github.com/Oleaintueri/Conjure/pkg/handler"
+	"github.com/urfave/cli/v2"
+	"log"
+	"os"
 )
 
-func main() {
-	h, err := handler.New(handler.WithFile("example/conjure.yml", nil), handler.WithParser(handler.Yaml), handler.WithFileType(handler.FilePath))
+func load(conjureFile string) error {
+	h, err := handler.New(handler.WithFile(conjureFile, nil), handler.WithParser(handler.Yaml), handler.WithFileType(handler.FilePath))
 
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	h, err = h.BuildConjureFile()
@@ -21,10 +25,43 @@ func main() {
 	}
 
 	if err = c.Recall(); err != nil {
-		panic(err)
+		return err
 	}
 
+	log.Println("Successfully recalled values...")
+
 	if err = c.WriteFiles(); err != nil {
-		panic(err)
+		return err
+	}
+
+	log.Println("Successfully wrote values...")
+
+	return nil
+}
+
+func main() {
+	var conjureFile string
+
+	app := &cli.App{
+		Name: "Conjure",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "source",
+				Aliases:     []string{"s"},
+				Usage:       "Load your conjure file",
+				Destination: &conjureFile,
+			},
+		},
+		Action: func(context *cli.Context) error {
+			if conjureFile == "" {
+				return fmt.Errorf("`--source` flag cannot be empty")
+			}
+			return load(conjureFile)
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
